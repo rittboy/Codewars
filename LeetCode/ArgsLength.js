0d24b275bd6147bcbef5d399e94659292cf85e28
John Ritter <john.ritter.8788@gmail.com>
2023-10-26 07:54:01 -0400

    Adding more coding challenges to repo

---
+++ LeetCode/ArgsLength.js
@@ -0,0 +1,18 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+
+var argumentsLength = function(...args) {
+	let i = 0;
+  while(i < args.length){
+    i++;
+  }
+
+  return i;
+};
+let length = argumentsLength(1, 15, "Alpha", 27);
+console.log(length)
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
