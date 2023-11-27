70b59624a5fda51950205cab0dd84bdfde18005e
John Ritter <john.ritter.8788@gmail.com>
2023-11-27 18:44:45 -0500

    Started code for Rainfall Correllary test

---
+++ Other Tests/Rainfall.js
@@ -0,0 +1,10 @@
+/**
+ * This takes any given number,
+ * and if the number is even, it will divide the number by two,
+ * and if it is odd, it will multiply it by three and add one. 
+ * This function exits once x becomes 1, and then returns how many steps it took 
+ * to reduce down to 1
+ */
+function Rainfall(x){
+
+}
