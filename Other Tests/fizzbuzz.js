2098c5d1c2af6096419285573e1a1f079265a66d
John Ritter <john.ritter.8788@gmail.com>
2024-01-19 03:03:02 -0500

    Made formatting change to FizzBuzz to make results more readable

--- Other Tests/fizzbuzz.js
+++ Other Tests/fizzbuzz.js
@@ -2,6 +2,9 @@ function fizzBuzz(start, end){
     let result = [];
 
     for(let i = start; i <= end; i++){
+        if(i % 50 === 0){
+            result.push(i + "\n");
+        }
         if(i % 3 === 0 && i % 5 === 0){
             result.push("FizzBuzz");
         }else if(i % 3 === 0){
